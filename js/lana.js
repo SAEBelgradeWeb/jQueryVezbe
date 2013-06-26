47873c047863511d6fecae4f2c6d9d30e29314aa
mullala <laninaposhta@gmail.com>
2013-06-26 18:01:59 +0200

    domaci (kako da includujemo jquery ako ne menjamo html fajl?)

---
+++ js/lana.js
@@ -0,0 +1,50 @@
+//ZADATAK 1
+
+// $(document).ready(function(){
+// 	$('li').addClass('green');
+// });
+
+
+//ZADATAK 2
+
+//$(document).ready(function(){
+//	$('li:nth-child(2)').addClass('green');
+//ILI
+//	$('.lista').children().eq(1).addClass('green');
+//});
+
+
+//ZADATAK 3
+
+// $(document).ready(function(){
+// 	$('p').after('<h2>Novi naslov</h2>');
+// });
+
+
+//ZADATAK 4
+
+// $(document).ready(function(){
+// 	$('<li>podstavka 0</li>').prependTo('li > ul:first-Child');
+// });
+
+
+//ZADATAK 5
+
+// $(document).ready(function(){
+// 	$('.lista').children().eq(2).siblings().addClass('green');
+// });
+
+
+//ZADATAK 6
+//Ne valja ti encoding
+
+// $(document).ready(function(){
+// 	$('h1').html('NASLOV JAŠTA');
+// });
+
+
+//ZADATAK 7
+
+// $(document).ready(function(){
+// 	$('p').prepend('Ovo je moja fantastična rečenica. ');
+// });
