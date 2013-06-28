0b78ea98a7e0b0e4492d1669d9fb3396224e6a4d
salebgmilj2 <aleksandar.salebg@gmail.com>
2013-06-28 14:12:32 +0200

    Uradio domaci, morao da include-jem jquery u HTML

---
+++ js/aleksandar.js
@@ -0,0 +1,42 @@
+/* ZADATAK 1
+
+$(document).ready(function(){
+	$('li').addClass('green');
+});*/
+
+  // ZADATAK 2
+
+ /*$(document).ready(function(){
+ 	//$('li:nth-child(2)').addClass('green');
+ 	$('.lista').children().eq('1').addClass('green');
+ });*/
+
+ /* // ZADATAK 3
+
+ $(document).ready(function(){
+ 	$('p').after('<h2>Ovo je naslov Posle paragrafa</h2>')
+ });*/
+
+ /*//ZADATAK 4
+
+ $(document).ready(function(){
+ 	$('ul:eq(1)').prepend('<li>podstavka 0</li>');
+ });*/
+
+/*// ZADATAK 5
+
+$(document).ready(function(){
+	$('.lista').children().eq('2').siblings().addClass('green');
+});*/
+
+/*//ZADATAK 6
+
+$(document).ready(function(){
+	$('h1').text('Ovde je bio "Naslov"');
+});*/
+
+/*//ZADATAK 7
+
+$(document).ready(function(){
+	$('p').prepend('Ovo je moja recenica ubacena na pocetak paragrafa <br>');
+});*/
